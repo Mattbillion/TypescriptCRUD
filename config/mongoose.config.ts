e49ce553244749d3b28078354b4ad8cff65cf970
Munkhbat Enkhtuya <muugiiyep0722@gmail.com>
2023-04-05 09:43:11 +0800

    added

--- config/mongoose.config.ts
+++ config/mongoose.config.ts
@@ -1,13 +1,3 @@
 import mongoose from "mongoose";
 import * as dotenv from "dotenv"
 
-const port = process.env.DB_CONN_STRING;
-
-mongoose
-  .connect(port, {
-    useCreateIndex: true,
-    newNewUrlParser: true,
-    useUnifiedTopology: true
-  }, () => {
-    console.log("connected to DATABASE!")
-  })
